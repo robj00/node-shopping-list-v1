bda33a72e6303fc51245193f11b133aeda56914d
Rob Palmer <rjpalmer15@gmail.com>
2017-06-26 13:47:45 -0400

    recipes git complete

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
 const {ShoppingList} = require('./models');
+const {Recipes} = require('./models');
 
 const jsonParser = bodyParser.json();
 const app = express();
@@ -17,11 +18,19 @@ app.use(morgan('common'));
 ShoppingList.create('beans', 2);
 ShoppingList.create('tomatoes', 3);
 ShoppingList.create('peppers', 4);
+Recipes.create('chocolate milk', ['cocoa', 'milk' , 'sugar']);
+Recipes.create('yogurt snack', ['greek yogurt', 'protein powder' , 'frozen blueberries']);
+Recipes.create('cereal', ['go lean', 'all bran' , 'soy milk']);
+
 
 // when the root of this router is called with GET, return
 // all current ShoppingList items
 app.get('/shopping-list', (req, res) => {
   res.json(ShoppingList.get());
+});
+
+app.get('/recipes', (req, res) => {
+ 	res.json(Recipes.get());
 });
 
 app.listen(process.env.PORT || 8080, () => {
